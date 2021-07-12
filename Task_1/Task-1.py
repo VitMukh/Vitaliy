97a7c74a85cbe0ecf7703d23fb1104d299560d06
VitMukh <86917979+VitMukh@users.noreply.github.com>
2021-07-12 12:44:43 +0300

    Update Task-1.py

--- Task_1/Task-1.py
+++ Task_1/Task-1.py
@@ -5,12 +5,13 @@ letters = ''.join([i for i in string if not i.isdigit()])
 nums = re.findall(r'\d+', string)
 nums = [int(i) for i in nums]
 print("\nРядок без чисел:", letters)
-print("Масив чисел:", nums)
-
 LetterS = ' '.join(word[0].upper() + word[1:-1] + word[-1:].upper() for word in letters.split())
-print("\nЗмінений рядок:", LetterS)
-print("Максимальний елемент масиву:", max(nums))
-nums.remove(max(nums))
-nums_with_index = [nums[i]**i for i in range(0, len(nums))]
-print("Масив чисел, піднесені до степеню по їх індексу:", nums_with_index)
+print("Змінений рядок:", LetterS)
+print("\nМасив чисел:", nums)
+if len(nums) == 0:
+    print("Масив не містить чисел, подальші операції не можуть бути проведені")
+else:
+    print("Максимальний елемент масиву:", max(nums))
+    nums_with_index = [nums[i]**(i+1) for i in range(0, len(nums))]
+    print("Масив чисел, піднесені до степеню по їх індексу:", nums_with_index)
 print("\n")
